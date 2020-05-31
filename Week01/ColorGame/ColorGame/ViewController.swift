22cbebd71966d21a087fdccb21c450828426c172
alexandranavruzova <haughtygrumbler@gmail.com>
2020-05-31 10:24:01 +0300

    Added a sliderMoved function that updates sliders' labels

--- Week01/ColorGame/ColorGame/ViewController.swift
+++ Week01/ColorGame/ColorGame/ViewController.swift
@@ -12,23 +12,44 @@ class ViewController: UIViewController {
     
     //Mark: Properties
     var currentValue = 0
-    
+    var redValue = 0
+    var greenValue = 0
+    var blueValue = 0
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+
+    }
+    
     //MARK: Actions
-    // Keeps track of the current value of the slider and converts it to an Integer
+    
+    // Update sliders' labels
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        switch slider {
+        case redSlider:
+            redLabel.text = String(currentValue)
+            redValue = currentValue
+        case greenSlider:
+            greenLabel.text = String(currentValue)
+            greenValue = currentValue
+        case blueSlider:
+            blueLabel.text = String(currentValue)
+            blueValue = currentValue
+        default:
+            print("i don't know what's that")
+        }
     }
+    
 }
 
